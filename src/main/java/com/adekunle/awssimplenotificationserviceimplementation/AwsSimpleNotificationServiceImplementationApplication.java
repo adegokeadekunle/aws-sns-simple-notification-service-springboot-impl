86a1a49eedc4f64ba86b7512e082b03966b2a7d6
adegokeadekunle2 <adegokeadekunle2@gmail.com>
2023-01-04 23:46:37 +0100

    sns initial

---
+++ src/main/java/com/adekunle/awssimplenotificationserviceimplementation/AwsSimpleNotificationServiceImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.adekunle.awssimplenotificationserviceimplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSimpleNotificationServiceImplementationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsSimpleNotificationServiceImplementationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adekunle/awssimplenotificationserviceimplementation/AwsSimpleNotificationServiceImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adekunle.awssimplenotificationserviceimplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSimpleNotificationServiceImplementationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
